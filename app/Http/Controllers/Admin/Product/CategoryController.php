2f2afd844f692ecbb17dc26b47eb4ddf4463c51d
JimmyCablIni <jaimecaballero99@gmail.com>
2024-07-09 12:45:07 +0200

    Desarrollo del CategoryController.php

--- app/Http/Controllers/Admin/Product/CategoryController.php
+++ app/Http/Controllers/Admin/Product/CategoryController.php
@@ -3,16 +3,25 @@
 namespace App\Http\Controllers\Admin\Product;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $search = $request->search;
+
+        $categories = Category::where('name', 'LIKE', '%' . $search . '%')->orderBy("id", "desc")->paginate(25);
+
+        return response()->json([
+            "total" => $categories->total(),
+            "categories" => $categories,
+        ]);
     }
 
     /**
@@ -20,7 +29,17 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $is_exists = Category::where("name", $request->name)->first();
+        if($is_exists) {
+            return response()->json(["message" => 403]);
+        }
+        if ($request->hasFile('image')()) {
+            $path = Storage::putFile("categories", $request->file('image'));
+            $request->request->add(["imagen" => $path]);
+        }
+        $category = Category::create($request->all());
+
+        return response()->json(["message" => 200]);
     }
 
     /**
@@ -28,7 +47,8 @@ class CategoryController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        return response()->json(["category" => $category]);
     }
 
     /**
@@ -36,7 +56,20 @@ class CategoryController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $is_exists = Category::where("id",'<>', $id)->where("name", $request->name)->first();
+        if($is_exists) {
+            return response()->json(["message" => 403]);
+        }
+        $category = Category::findOrFail($id);
+        if($request->hasFile('image')()) {
+            if ($category->imagen) {
+                Storage::delete($category->imagen);
+            }
+            $path = Storage::putFile("categories", $request->file('image'));
+            $request->request->add(["imagen" => $path]);
+        }
+        $category->update($request->all());
+        return response()->json(["message" => 200]);
     }
 
     /**
@@ -44,6 +77,9 @@ class CategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return response()->json(["message" => 200]);
     }
 }
